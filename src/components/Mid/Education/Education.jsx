d2fa2bbff67bb69e38f8d451c1faa4320bb4ac0f
yuvalelarat <yuvalelarat@gmail.com>
2024-11-14 23:52:14 +0200

    minor fixes

--- src/components/Mid/Education/Education.jsx
+++ src/components/Mid/Education/Education.jsx
@@ -1,14 +1,15 @@
 import "./Education.css";
 
-function Education({style}) {
-
+function Education({ style }) {
   return (
     <div>
-    <h4 style={style}>Education:</h4>
-    <ul>
-      <li>Bsc Computer Science - HIT (3rd year)</li>
-      <li>BA Economics & Management (graduate)</li>
-    </ul>
+      <h4 style={style}>Education:</h4>
+      <ul>
+        <li>BSc Computer Science - HIT (3rd year)</li>
+        <li>
+          BA Economics & Management - Bar Ilan university (graduate)
+        </li>
+      </ul>
     </div>
   );
 }
