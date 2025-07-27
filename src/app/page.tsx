7d40249e9f47b569944f1aece7b7db16f95b16c4
‪Yuval Elarat‬‏ <yuvalelarat@gmail.com>
2025-07-27 16:15:21 +0300

    comment out toast for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 'use client';
-import { useEffect } from "react";
-import { toast } from "react-hot-toast";
+// import { useEffect } from "react";
+// import { toast } from "react-hot-toast";
 import Header from "./layouts/header";
 import Main from "./layouts/main";
 
 export default function Home() {
-  useEffect(() => {
-    toast(
-      "This Portfolio is still under construction.\nSome sections may be incomplete.",
-      {
-        duration: 6000,
-        icon: '🚧',
-      }
-    );
-  }, []);
+  // useEffect(() => {
+  //   toast(
+  //     "This Portfolio is still under construction.\nSome sections may be incomplete.",
+  //     {
+  //       duration: 6000,
+  //       icon: '🚧',
+  //     }
+  //   );
+  // }, []);
 
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
